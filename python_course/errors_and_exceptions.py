23cff489acfd13098e15a09cbfd11c20a38b7561
MahmoudBarakat <eng.barakat11@gmail.com>
2025-11-24 22:39:22 +0200

    [ADD] erros and exceptions

---
+++ python_course/errors_and_exceptions.py
@@ -0,0 +1,25 @@
+###
+#
+#
+# Errors and Exceptions
+#
+#
+###
+
+# age = int(input("Please Enter Your Age: "))
+# if age < 18:
+#     raise ValueError("Your Age must be bigger than 18 Years!!")
+
+# try:
+#     age = int(input("Please Enter Your Age: "))
+# except:
+#     print("Please Enter Only Number!!")
+
+# def check_age(age):
+#     if age < 0:
+#         raise ValueError("Age cannot be negative")
+#     return age
+# try:
+#     check_age(-5)
+# except ValueError as err:
+#     print(err)
