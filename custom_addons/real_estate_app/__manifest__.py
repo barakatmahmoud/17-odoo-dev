abf11b1db19c57226b8bda3dab0ec6136cf5abea
MahmoudBarakat <eng.barakat11@gmail.com>
2025-11-28 20:27:06 +0200

    [ADD] new app

---
+++ custom_addons/real_estate_app/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'version' : '17.1',
+    'summary': 'Custom App Real Estate For Learning',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/base_menus.xml',
+        'views/property_views.xml',
+    ],
+    'application': True,
+}
