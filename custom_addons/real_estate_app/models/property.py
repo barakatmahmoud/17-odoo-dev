b5a4b0c925d6162fd7c43818f9b7802b0a43670d
MahmoudBarakat <eng.barakat11@gmail.com>
2025-12-07 17:52:15 +0200

    [ADD] new module

--- custom_addons/real_estate_app/models/property.py
+++ custom_addons/real_estate_app/models/property.py
@@ -1,15 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Property(models.Model):
     _name = 'property'
 
-    name = fields.Char()
+    name = fields.Char(default='New')
     description = fields.Text()
-    post_code = fields.Char()
+    post_code = fields.Char(required=True)
     date_availability = fields.Date()
     expected_price = fields.Float()
     selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(required=True)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage =fields.Boolean()
@@ -21,3 +22,16 @@ class Property(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ])
+
+    ### ADD SQL Constraints ###
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'The property name must be unique!')
+    ]
+
+    ### ADD Logic or Python Validation ###
+    @api.constrains('bedrooms')
+    def _check_bedroom(self):
+        for rec in self:
+            if rec.bedrooms <=0:
+                raise ValidationError("Please, Enter Valid Number Of Bedrooms")
+
