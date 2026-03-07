df214c318af2ebcc98960bd5c11fc29684349ffb
MahmoudBarakat <eng.barakat11@gmail.com>
2026-03-08 01:34:35 +0200

    [Solve] Find the Runner-Up Score!

--- python_course/problem_solving.py
+++ python_course/problem_solving.py
@@ -1,12 +1,20 @@
 ### List Comprehensions
-x = int(input("Please enter a number"))
-y = int(input("Please enter a number"))
-z = int(input("Please enter a number"))
-n = int(input("Please enter a number"))
-result = []
-for i in range(x+1):
-    for j in range(y+1):
-        for k in range(z+1):
-            if i+j+k != n:
-                result.append([i,j,k])
-print(result)
+# x = int(input("Please enter a number"))
+# y = int(input("Please enter a number"))
+# z = int(input("Please enter a number"))
+# n = int(input("Please enter a number"))
+# result = []
+# for i in range(x+1):
+#     for j in range(y+1):
+#         for k in range(z+1):
+#             if i+j+k != n:
+#                 result.append([i,j,k])
+# print(result)
+
+
+### Find the Runner-Up Score!
+# n = int(input())
+# numbers = list(map(int, input().split()))
+# numbers = list(set(numbers))   # إزالة التكرار
+# numbers.sort()                 # ترتيب القائمة
+# print(numbers[-2])             # ثاني أكبر عنصر
