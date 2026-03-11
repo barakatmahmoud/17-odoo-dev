5bc25a1e83bd23f7b546a63c2dbc0c205810055a
MahmoudBarakat <eng.barakat11@gmail.com>
2026-03-12 00:43:45 +0200

    [Add] lists

--- python_course/problem_solving.py
+++ python_course/problem_solving.py
@@ -63,3 +63,30 @@
 # marks = student_marks[query_name]
 # average = sum(marks) / len(marks)
 # print(f"{average:.2f}")
+
+###Lists
+# n = int(input("Enter Number Of Commands: "))
+# my_list = []
+#
+# for _ in range(n):
+#     command = input("Enter Command").split()
+#
+#     if command[0] == "insert":
+#         i = int(command[1])
+#         e = int(command[2])
+#         my_list.insert(i, e)
+#     elif command[0] == "print":
+#         print(my_list)
+#     elif command[0] == "remove":
+#         e = int(command[1])
+#         my_list.remove(e)
+#     elif command[0] == "append":
+#         e = int(command[1])
+#         my_list.append(e)
+#     elif command[0] == "sort":
+#         my_list.sort()
+#     elif command[0] == "pop":
+#         my_list.pop()
+#     elif command[0] == "reverse":
+#         my_list.reverse()
+
