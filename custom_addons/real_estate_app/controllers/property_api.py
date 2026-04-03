a8f9230b5c3d8f1bb89b7e080f09b9b17246d223
MahmoudBarakat <eng.barakat11@gmail.com>
2026-04-03 05:04:25 +0200

    [ADD] Add update operation api

--- custom_addons/real_estate_app/controllers/property_api.py
+++ custom_addons/real_estate_app/controllers/property_api.py
@@ -59,3 +59,35 @@ class PropertyApi(http.Controller):
                     'message': 'Property Created',
                 }
             )
+
+    ###WRITE OPERATION
+    @http.route('/update/property/<int:property_id>', methods=['PUT'], type='http', auth='none', csrf=False)
+    def update_property(self, property_id):
+       try:
+           property_id = request.env['property'].sudo().search([('id', '=', property_id)])
+           ##Check ID is right
+           if not property_id:
+               return request.make_json_response(
+                   {
+                       'message': 'ID IS INCORRECT',
+                   }, status=400
+               )
+           args = request.httprequest.data.decode()
+           print("ARGS", args)
+           ## transfer json to dictionary
+           vals = json.loads(args)
+           print("VALS", vals)
+           ## update data
+           property_id.write(vals)
+           return request.make_json_response(
+               {
+                   'message': 'Property Updated',
+                   'name': property_id.name,
+               }, status=200
+           )
+       except Exception as error:
+           return request.make_json_response(
+               {
+                   'message': error,
+               }, status=400
+           )
