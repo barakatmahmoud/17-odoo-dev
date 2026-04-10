2e9ee2a6905e7defbeb9b46644d772a8d0aa1bcd
MahmoudBarakat <eng.barakat11@gmail.com>
2026-04-10 10:57:54 +0200

    [ADD] Add intro js course

---
+++ js_course/main.js
@@ -0,0 +1,13 @@
+// document >> HTML
+document.write('Hello World!');
+
+// console >> Console
+console.log("Hello World!!");
+
+// window >> Browser
+// window.alert("Hello World!!!");
+
+// Concatenation
+let name = 'Mahmoud';
+let age = 28;
+console.log(`My Name Is ${name} and my age is ${age}`);
