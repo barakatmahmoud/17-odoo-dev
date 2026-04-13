9d4ef01abb3b1d0154cf2650895abd3d7a7a6bc0
MahmoudBarakat <eng.barakat11@gmail.com>
2026-04-13 15:24:01 +0200

    [ADD] Add data type

--- js_course/main.js
+++ js_course/main.js
@@ -1,13 +1,55 @@
 // document >> HTML
-document.write('Hello World!');
+// document.write('Hello World!');
 
 // console >> Console
-console.log("Hello World!!");
+// console.log("Hello World!!");
 
 // window >> Browser
 // window.alert("Hello World!!!");
 
 // Concatenation
-let name = 'Mahmoud';
-let age = 28;
-console.log(`My Name Is ${name} and my age is ${age}`);
+// let name = 'Mahmoud';
+// let age = 28;
+// console.log(`My Name Is ${name} and my age is ${age}`);
+
+// Calc Zakah
+// let zakah = prompt("Enter Your Money");
+// console.log(0.025 * zakah + "جنية");
+
+// If statement
+// let  age = prompt("Enter Your Age:")
+// let result =  age > 18? console.log("Hello")
+//     : age == 18? console.log("You are 18 now!!")
+//         : console.log("You are Very young");
+
+
+// switch statement
+// let role = prompt("Enter Your Role:");
+// switch (role.toLowerCase())
+// {
+//     case 'admin':
+//         document.write("Create, Update, Delete");
+//         break;
+//     case 'editor':
+//          document.write("Create, Update");
+//          break;
+//     default:
+//         document.write("Hello Normal User");
+// }
+
+
+// Function
+function calcAgeByDays(age){
+    let result = age * 365;
+    return result
+}
+let ageToDays = calcAgeByDays(28);
+console.log(`Age By Days ${ageToDays}`);
+
+
+function clacAgeByHours(age){
+    let result = age * 24;
+    return `Age By Hours ${result}`;
+}
+let ageToHours = clacAgeByHours(ageToDays);
+console.log(ageToHours);
