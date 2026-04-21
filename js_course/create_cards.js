7348841feae9b8fcf62c56014bd25d20d9d5c964
MahmoudBarakat <eng.barakat11@gmail.com>
2026-04-21 13:05:12 +0200

    [ADD] add create cards use js

---
+++ js_course/create_cards.js
@@ -0,0 +1,39 @@
+// create cards use js pure
+// let container = document.createElement('div');
+// names = ['ahmed', 'sara', 'mazen', 'nadin'];
+// ages = ['18 years old', '20 years old', '40 years old', '13 years old']
+// document.body.appendChild(container);
+// container.style.textAlign = 'center';
+//
+// function element(userName, userAge){
+//
+//     //elements
+//     let card = document.createElement('div');
+//     let title =  document.createElement('h2');
+//     let age =  document.createElement('p');
+//     let img =  document.createElement('img');
+//
+//     //content
+//     let name = document.createTextNode(userName);
+//     let ageContent = document.createTextNode(userAge);
+//     img.src = 'images/icon.png';
+//
+//     title.appendChild(name);
+//     age.appendChild(ageContent);
+//
+//     //style
+//     card.style.width = '200px';
+//     card.style.display = 'inline-block';
+//     card.style.margin = '2px';
+//     img.style.width = '100%';
+//
+//     card.appendChild(title);
+//     card.appendChild(age);
+//     card.appendChild(img);
+//
+//     container.appendChild(card);
+//
+// }
+// for(let i=0; i<4; i++){
+//     element(names[i], ages[i]);
+// }
--- js_course/main.js
+++ js_course/main.js
@@ -38,18 +38,109 @@
 // }
 
 
-// Function
-function calcAgeByDays(age){
-    let result = age * 365;
-    return result
-}
-let ageToDays = calcAgeByDays(28);
-console.log(`Age By Days ${ageToDays}`);
+// Function convert age to days
+// function calcAgeByDays(age){
+//     let result = age * 365;
+//     return result
+// }
+// let ageToDays = calcAgeByDays(28);
+// console.log(`Age By Days ${ageToDays}`);
+//
+//
+// function clacAgeByHours(age){
+//     let result = age * 24;
+//     return `Age By Hours ${result}`;
+// }
+// let ageToHours = clacAgeByHours(ageToDays);
+// console.log(ageToHours);
+
+// object in js
+// let car = {
+//     'name' : 'BMW',
+//     'price': 50000,
+//     'color': ['red', 'blue', 'green'],
+//
+//     hello:function (){
+//         return 'Hello';
+//     }
+// }
+// console.log(car.hello());
 
 
-function clacAgeByHours(age){
-    let result = age * 24;
-    return `Age By Hours ${result}`;
-}
-let ageToHours = clacAgeByHours(ageToDays);
-console.log(ageToHours);
+// this use with object
+// let user = {
+//     'name' : 'Mahmoud',
+//
+//     sayName : function (){
+//         return this.name
+//     }
+// }
+// console.log(user.sayName())
+
+// this global >> window
+// let x = this;
+// console.log(this);
+
+// this use with function >> window
+// function hello(){
+//     return this;
+// }
+// console.log(hello());
+
+//// Dom
+
+// get element by Id
+// let headId = document.getElementById('head');
+// console.log(headId);
+
+// get element by ClassName >> return list
+// let headClass = document.getElementsByClassName('header')[0];
+// console.log(headClass);
+
+// get element by TagName >> return list
+// let headTag = document.getElementsByTagName('h3')[1];
+// console.log(headTag)
+
+// get element by query selector >> #id
+// let headIdQuery = document.querySelector('#head');
+// console.log(headIdQuery);
+
+// get element by query selector >> .class >> return first class has this class
+// let headClassQuery = document.querySelector('.header');
+// console.log(headClassQuery);
+
+// get element by query selector >> tag >> return first tag has this tag
+// let headTagQuery = document.querySelector('h3');
+// console.log(headTagQuery);
+
+// get element by query selector all use with class and tag >> return list
+// let headClassQueryAll = document.querySelectorAll('.header');
+// console.log(headClassQueryAll);
+
+// get html body
+// let body = document.body;
+// console.log(body);
+
+// get html form
+// let form = document.forms;
+// console.log(form);
+
+
+// element = document.getElementById('head');
+// get previous element
+// console.log(element.previousElementSibling);
+// get previous
+// console.log(element.previousSibling);
+// get parent
+// console.log(element.parentElement);
+
+
+// element = document.getElementById('container');
+// inner html
+// element.innerHTML = '<h3>Hello By Js</h3>';
+// outer html
+// element.outerHTML = '<h3>Hello By Js</h3>';
+// inner text
+// element.innerText = "Hello Jsss";
+// outer text
+// element.outerText = "Hello Jsss";
