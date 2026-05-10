a23e0026544f7d5c16107e6cfd45350e0cc3b175
MahmoudBarakat <eng.barakat11@gmail.com>
2026-05-10 23:55:25 +0300

    [ADD] get/set element and att

--- js_course/jquery.js
+++ js_course/jquery.js
@@ -43,3 +43,48 @@
 //         });
 //     })
 // })
+
+// get and set text
+// $(document).ready(function(){
+//     var div = $('div').text();
+//     $('p').text(div);
+// })
+
+// get and set html
+// $(document).ready(function(){
+//     var div = $('div').html();
+//     $('p').text(div);
+// })
+
+// get and set val
+// $(document).ready(function (){
+//     $('button').click(function (){
+//         $('input').val("Barakat");
+//     })
+// })
+
+// check input is empty or not use val (check required field)
+// $(document).ready(function (){
+//     $('#check_btn').click(function (){
+//         if ($('#name').val() == ''){
+//             $('div').text('You Must Enter Value');
+//         }
+//         else{
+//             $('div').text('');
+//         }
+//     })
+// })
+
+// get attr
+// $(document).ready(function (){
+//     $('button').click(function (){
+//         $('input').val($('div').attr('class'));
+//     })
+// })
+
+// set attr
+// $(document).ready(function (){
+//     $('button').click(function (){
+//         $('div').attr('class', 'new_class');
+//     })
+// })
